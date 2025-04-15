34703378cc0964b882ff8d5f670430b6ab74f909
Bohdan Kaftanatii <bogdankaftanatiy@gmail.com>
2025-04-15 21:10:46 +0200

    changed csv logger to autoclosing

--- src/qubx/core/loggers.py
+++ src/qubx/core/loggers.py
@@ -122,20 +122,14 @@ class CsvFileLogsWriter(LogsWriter):
         # - it rewrites positions every time
         self._pos_file_path = f"{path}/{self.strategy_id}_{self.account_id}_positions.csv"
         self._balance_file_path = f"{path}/{self.strategy_id}_{self.account_id}_balance.csv"
+        self._portfolio_file_path = f"{path}/{self.strategy_id}_{self.account_id}_portfolio.csv"
+        self._execs_file_path = f"{path}/{self.strategy_id}_{self.account_id}_executions.csv"
+        self._signals_file_path = f"{path}/{self.strategy_id}_{self.account_id}_signals.csv"
 
-        _pfl_path = f"{path}/{strategy_id}_{account_id}_portfolio.csv"
-        _exe_path = f"{path}/{strategy_id}_{account_id}_executions.csv"
-        _sig_path = f"{path}/{strategy_id}_{account_id}_signals.csv"
-        self._hdr_pfl = not os.path.exists(_pfl_path)
-        self._hdr_exe = not os.path.exists(_exe_path)
-        self._hdr_sig = not os.path.exists(_sig_path)
+        self._hdr_pfl = not os.path.exists(self._portfolio_file_path)
+        self._hdr_exe = not os.path.exists(self._execs_file_path)
+        self._hdr_sig = not os.path.exists(self._signals_file_path)
 
-        self._pfl_file_ = open(_pfl_path, "+a", newline="")
-        self._execs_file_ = open(_exe_path, "+a", newline="")
-        self._sig_file_ = open(_sig_path, "+a", newline="")
-        self._pfl_writer = csv.writer(self._pfl_file_)
-        self._exe_writer = csv.writer(self._execs_file_)
-        self._sig_writer = csv.writer(self._sig_file_)
         self.pool = ThreadPool(3)
 
     @staticmethod
@@ -155,25 +149,28 @@ class CsvFileLogsWriter(LogsWriter):
                     w.writerows(self._values(data))
 
             case "portfolio":
-                if self._hdr_pfl:
-                    self._pfl_writer.writerow(self._header(data[0]))
-                    self._hdr_pfl = False
-                self._pfl_writer.writerows(self._values(data))
-                self._pfl_file_.flush()
+                with open(self._portfolio_file_path, "w", newline="") as f:
+                    w = csv.writer(f)
+                    if self._hdr_pfl:
+                        w.writerow(self._header(data[0]))
+                        self._hdr_pfl = False
+                    w.writerows(self._values(data))
 
             case "executions":
-                if self._hdr_exe:
-                    self._exe_writer.writerow(self._header(data[0]))
-                    self._hdr_exe = False
-                self._exe_writer.writerows(self._values(data))
-                self._execs_file_.flush()
+                with open(self._execs_file_path, "w", newline="") as f:
+                    w = csv.writer(f)
+                    if self._hdr_exe:
+                        w.writerow(self._header(data[0]))
+                        self._hdr_exe = False
+                    w.writerows(self._values(data))
 
             case "signals":
-                if self._hdr_sig:
-                    self._sig_writer.writerow(self._header(data[0]))
-                    self._hdr_sig = False
-                self._sig_writer.writerows(self._values(data))
-                self._sig_file_.flush()
+                with open(self._signals_file_path, "w", newline="") as f:
+                    w = csv.writer(f)
+                    if self._hdr_sig:
+                        w.writerow(self._header(data[0]))
+                        self._hdr_sig = False
+                    w.writerows(self._values(data))
 
             case "balance":
                 with open(self._balance_file_path, "w", newline="") as f:
@@ -186,17 +183,9 @@ class CsvFileLogsWriter(LogsWriter):
             self.pool.apply_async(self._do_write, (log_type, data))
 
     def flush_data(self):
-        try:
-            self._pfl_file_.flush()
-            self._execs_file_.flush()
-            self._sig_file_.flush()
-        except Exception as e:
-            logger.warning(f"Error flushing log writer: {str(e)}")
+        pass
 
     def close(self):
-        self._pfl_file_.close()
-        self._execs_file_.close()
-        self._sig_file_.close()
         self.pool.close()
         self.pool.join()
 
