05514d7ea7e6b7c312bc4078d71ffebb8d4851b3
quant0 <dmitry.ema@gmail.com>
2025-05-23 11:00:08 +0000

    feat: add rolling_zscore to ta.py

--- src/qubx/pandaz/ta.py
+++ src/qubx/pandaz/ta.py
@@ -633,6 +633,17 @@ def rolling_std_with_mean(x: pd.Series, mean: float | pd.Series, window: int):
     return np.sqrt((((x - mean) ** 2).rolling(window=window).sum() / (window - 1)))
 
 
+def rolling_zscore(x: pd.Series, window: int):
+    """
+    Calculates rolling z-score for data from x
+    :param x: series data
+    :param window: window
+    :return: rolling z-score
+    """
+    r = x.rolling(window=window)
+    return (x - r.mean()) / r.std(ddof=0)
+
+
 def bollinger(x: pd.Series, window=14, nstd=2, mean="sma") -> pd.DataFrame:
     """
     Bollinger Bands indicator
