761fa600d674a259df2ad1a548e102eb27a99d9e
quant0 <dmitry.ema@gmail.com>
2025-10-02 16:42:03 +0000

    wip: QuestDB in progress

--- src/qubx/data/storages/questdb.py
+++ src/qubx/data/storages/questdb.py
@@ -25,17 +25,12 @@ def _retry(fn):
                 return fn(*args, **kw)
             except (pg.InterfaceError, pg.OperationalError, AttributeError):
                 logger.debug("Database Connection [InterfaceError or OperationalError]")
-                # time.sleep(cls._reconnect_idle)
                 cls._connect()
 
     return wrapper
 
 
-class QuestDBReader(IReader):
-    pass
-
-
-class QuestDBStorage(IStorage):
+class PGConnectionHelper:
     _connection: pg.connection.Connection | None
     _reconnect_tries = 5
     _reconnect_idle = 0.1  # wait seconds before retying
@@ -91,8 +86,66 @@ class QuestDBStorage(IStorage):
         except:  # noqa: E722
             pass
 
-    def get_exchanges(self) -> list[str]: ...
 
-    def get_market_types(self, exchange: str) -> list[str]: ...
+_HACKED_RECODING = {
+    ("binance", "umswap"): ("binance.um", "swap"),
+    ("binance", "margin"): ("binance.pm", "swap"),
+    # ("coingecko", "fundamental"): ("coingecko", "coins"),
+}
 
-    def get_reader(self, exchange: str, market: str) -> IReader: ...
+
+class QuestDBReader(IReader):
+    def __init__(self, exchange: str, market: str, pgc: PGConnectionHelper) -> None:
+        self.exchange = exchange
+        self.market = market
+        self.pgc = pgc
+
+    def get_data_id(self, dtype: DataType | str = DataType.ALL) -> list[str] | dict[DataType, list[str]]:
+        # match dtype
+        ...
+
+
+class QuestDBStorage(IStorage, PGConnectionHelper):
+    pgc: PGConnectionHelper
+
+    def __init__(self, host="localhost", user="admin", password="quest", port=8812) -> None:
+        self.pgc = PGConnectionHelper(host, user, password, port)
+
+    @property
+    def connection_url(self):
+        return self.pgc.connection_url
+
+    def __getstate__(self):
+        return self.pgc.__getstate__()
+
+    def close(self):
+        self.pgc.close()
+
+    def __del__(self):
+        self.pgc.__del__()
+
+    def get_exchanges(self) -> list[str]:
+        return list(self._get_exch_info().keys())
+
+    def _get_exch_info(self) -> dict[str, dict[str, list[str]]]:
+        tables = self.pgc.execute("select table_name as name from tables()")[1]
+        exch_mkt_dtype: dict[str, dict[str, list[str]]] = defaultdict(lambda: defaultdict(list))
+        for (t,) in tables:
+            ss = t.split(".")
+            if len(ss) > 1:
+                exch, mkt, typ = ss[0], ss[1], (ss[2] if len(ss) > 2 else None)
+                exch, mkt = _HACKED_RECODING.get((exch, mkt), (exch, mkt))
+                exch_mkt_dtype[exch.upper()][mkt.upper()].append(typ or mkt.upper())
+        return exch_mkt_dtype  # todo: convert to normal dict
+
+    def get_market_types(self, exchange: str) -> list[str]:
+        exc_i = self._get_exch_info()
+        return list(exc_i.get(exchange.upper(), dict()).keys())
+
+    def get_reader(self, exchange: str, market: str) -> IReader:
+        e_info = self._get_exch_info()
+
+        if exchange in e_info and market in e_info[exchange]:
+            return QuestDBReader(exchange, market, self.pgc)
+
+        raise ValueError(f"Can't provide data reader for exchange {exchange} and market type {market}")
